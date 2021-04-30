94f58ae99db272c74e1b12232b5f65ed1b749c59
Axel Delafosse <axeldelafosse@gmail.com>
2021-04-30 21:04:46 +0200

    move react-native-web reset css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,42 +1,4 @@
-import Document, {
-  DocumentContext,
-  Html,
-  Head,
-  Main,
-  NextScript
-} from 'next/document';
-
-const style = `
-  /**
-   * Building on the RNWeb reset:
-   * https://github.com/necolas/react-native-web/blob/master/packages/react-native-web/src/exports/StyleSheet/initialRules.js
-   */
-  html, body, #__next {
-    width: 100%;
-    /* To smooth any scrolling behavior */
-    -webkit-overflow-scrolling: touch;
-    margin: 0px;
-    padding: 0px;
-    /* Allows content to fill the viewport and go beyond the bottom */
-    min-height: 100%;
-  }
-  html {
-    scroll-behavior: smooth;
-    /* Prevent text size change on orientation change https://gist.github.com/tfausak/2222823#file-ios-8-web-app-html-L138 */
-    -webkit-text-size-adjust: 100%;
-    height: 100%;
-  }
-  body {
-    display: flex;
-    /* Allows you to scroll below the viewport; default value is visible */
-    overflow-y: auto;
-    overscroll-behavior-y: none;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    -ms-overflow-style: scrollbar;
-  }
-`;
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
