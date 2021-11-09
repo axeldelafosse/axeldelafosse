7cd0c6829546f8baf8170162448da143218b9749
Axel Delafosse <axeldelafosse@gmail.com>
2021-11-09 15:00:49 +0100

    add custom dracula prism theme

--- src/components/code-block.tsx
+++ src/components/code-block.tsx
@@ -2,6 +2,71 @@
 
 import React, { ReactNode } from 'react';
 import Highlight, { defaultProps, Language } from 'prism-react-renderer';
+import type { PrismTheme } from 'prism-react-renderer';
+
+var theme: PrismTheme = {
+  plain: {
+    color: '#F8F8F2',
+    backgroundColor: '#282A36'
+  },
+  styles: [
+    {
+      types: ['prolog', 'constant', 'builtin'],
+      style: {
+        color: 'rgb(189, 147, 249)'
+      }
+    },
+    {
+      types: ['inserted', 'function'],
+      style: {
+        color: 'rgb(80, 250, 123)'
+      }
+    },
+    {
+      types: ['deleted'],
+      style: {
+        color: 'rgb(255, 85, 85)'
+      }
+    },
+    {
+      types: ['changed'],
+      style: {
+        color: 'rgb(255, 184, 108)'
+      }
+    },
+    {
+      types: ['punctuation', 'symbol'],
+      style: {
+        color: 'rgb(248, 248, 242)'
+      }
+    },
+    {
+      types: ['string', 'char', 'tag', 'selector'],
+      style: {
+        color: 'rgb(255, 121, 198)'
+      }
+    },
+    {
+      types: ['keyword', 'variable'],
+      style: {
+        color: 'rgb(189, 147, 249)'
+        // fontStyle: "italic",
+      }
+    },
+    {
+      types: ['comment'],
+      style: {
+        color: 'rgb(98, 114, 164)'
+      }
+    },
+    {
+      types: ['attr-name'],
+      style: {
+        color: 'rgb(241, 250, 140)'
+      }
+    }
+  ]
+};
 
 interface CodeBlockProps {
   className: string;
@@ -9,12 +74,25 @@ interface CodeBlockProps {
 }
 
 function CodeBlock({ className, children }: CodeBlockProps) {
-  const language = className.replace(/language-/, '') as Language;
+  const language = (className?.replace(/language-/, '') as Language) ?? 'tsx';
 
   return (
-    <Highlight {...defaultProps} code={children as string} language={language}>
+    <Highlight
+      {...defaultProps}
+      code={children as string}
+      language={language}
+      theme={theme}
+    >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px' }}>
+        <pre
+          className={className}
+          style={{
+            ...style,
+            paddingTop: '20px',
+            paddingLeft: '15px',
+            paddingRight: '15px'
+          }}
+        >
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
