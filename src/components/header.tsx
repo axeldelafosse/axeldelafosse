ba450beb74a0a1600201162b59f7ffb21115cf8c
Axel Delafosse <axeldelafosse@gmail.com>
2024-06-24 21:21:54 +0200

    Update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 function Header() {
   return (
-    <div className="h-16 w-screen flex items-center justify-end z-10 text-white">
+    <div className="h-16 w-screen flex items-center justify-center z-10 text-white">
       <Link href="/blog" passHref={true}>
         <a className="text-white no-underline">blog</a>
       </Link>
@@ -21,7 +21,7 @@ function Header() {
       </a>
       <span className="mx-2">•</span>
       <a
-        className="text-white no-underline mr-6"
+        className="text-white no-underline"
         href={`https://github.com/${process.env.NEXT_PUBLIC_ID}/${process.env.NEXT_PUBLIC_ID}`}
         target="_blank"
         rel="noopener noreferrer"
